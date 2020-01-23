2c8b765f4a68cb0fdcb5abd0c7ce34ca5f237a50
ThemosIO <hello@themos.io>
2020-01-23 13:01:51 +0200

    update

--- src/helpers/api.js
+++ src/helpers/api.js
@@ -2,11 +2,9 @@ import axios from 'axios';
 
 const api = axios.create({
   baseURL: 'https://telematics.oasa.gr/api',
-  timeout: 10000,
+  timeout: 15000,
   transformRequest: [data => JSON.stringify(data)],
   mode: 'no-cors',
-  withCredentials: true,
-  credentials: 'same-origin',
   headers: {
     'Accept': 'application/json',
     'Content-Type': 'application/json',
@@ -15,4 +13,4 @@ const api = axios.create({
 });
 
 export const GET_CLOSEST_STOPS = ({ lat, lon }) => lat && lon &&
-  api.get(`/?act=getClosestStops&p1=${lat}&p2=${lon}`);
+  api.post(`/?act=getClosestStops&p1=${lat}&p2=${lon}`);
