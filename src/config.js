5203f9143b66b9b65c3ba9c6afe939a815e0361f
ThemosIO <hello@themos.io>
2020-01-23 11:46:48 +0200

    https

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
-export const API = 'http://telematics.oasa.gr/api/';
+export const API = 'https://telematics.oasa.gr/api/';
 export const GET_CLOSEST_STOPS = ({ lat, lon }) => `${API}?act=getClosestStops&p1=${lat}&p2=${lon}`;
 export const GEO_OPTIONS = { enableHighAccuracy: true, timeout: 5000, maximumAge: 0 };
 export const GEO_ERRORS = { UNSUPPORTED: 'unsupported', FAILED: 'failed' };
