c9b5a021d40d4fd0472f99307c91ebc958c88bf3
ThemosIO <hello@themos.io>
2020-01-23 12:40:29 +0200

    update

--- src/helpers/api.js
+++ src/helpers/api.js
@@ -3,8 +3,10 @@ import axios from 'axios';
 const api = axios.create({
   baseURL: 'https://telematics.oasa.gr/api',
   timeout: 10000,
-  withCredentials: true,
   transformRequest: [data => JSON.stringify(data)],
+  mode: 'no-cors',
+  withCredentials: true,
+  credentials: 'same-origin',
   headers: {
     'Accept': 'application/json',
     'Content-Type': 'application/json',
