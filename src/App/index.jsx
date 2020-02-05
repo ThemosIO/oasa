82838c8487caf6e569884c7160972990c75fef44
ThemosIO <hello@themos.io>
2020-02-05 09:21:25 +0200

    updates

--- src/App/index.jsx
+++ src/App/index.jsx
@@ -1,16 +1,12 @@
 import throttle from 'lodash/throttle';
 import React, { useCallback, useEffect, useMemo, useState } from 'react';
-import Map from './Map';
-import { getStoredStop, getStoredRoutes } from '../helpers/localStorage';
+import { getStoredRoutes } from '../helpers/localStorage';
 import { GET_ROUTES, GET_ARRIVALS } from '../helpers/api';
 import s from './index.module.scss';
 
+const stop = { id: '420200', title: '7η Στάση' };
 const obj = {};
 const arr = [];
-
-// TODO: create clock component that countdowns to refresh requests
-// TODO: bus route stops as a line
-// TODO: Bottom part of screen is topleft: title, right: arrivals, bottomLeft: requests
 
 const Index = () => {
   const [updateTimestamp, setUpdateTimestamp] = useState('');
@@ -18,54 +14,22 @@ const Index = () => {
   const [apiRouteError, setApiRouteError] = useState(null);
   const [arrivals, setArrivals] = useState([]);
   const [routes, setRoutes] = useState(getStoredRoutes());
-  const [stopId, setStopId] = useState(null);
-  const [loadMap, setLoadMap] = useState(false);
-  const stop = useMemo(() => getStoredStop(stopId), [stopId]);
 
   const getRoutes = id => GET_ROUTES(id, setRoutes, setApiRouteError);
-  const throttledGetRoutes = useCallback(throttle(getRoutes, 5000), []);
+  const throttledGetRoutes = useCallback(throttle(getRoutes, 10000), []);
   const getArrivals = id => GET_ARRIVALS(id, setArrivals, setApiArrivalError, setUpdateTimestamp);
   const throttledGetArrivals = useCallback(throttle(getArrivals, 5000), []);
 
   useEffect(() => { // make api call again if error occurs.
-    if(!stopId) return;
     console.log('throttled useEffect');
-    apiArrivalError && throttledGetArrivals(stopId);
-    apiRouteError && throttledGetRoutes(stopId);
-  }, [apiArrivalError, apiRouteError, stopId]);
-
-  useEffect(() => { // make api call on new stopId
-    if(!stopId) return;
-    console.log('throttled useEffect (new id)');
-    throttledGetArrivals(stopId);
-    throttledGetRoutes(stopId);
-  }, [stopId]);
-
-  const setVh = () => document.documentElement.style.setProperty(
-    '--vh',
-    `${Math.max(document.documentElement.clientHeight, window.innerHeight, 0) * 0.01}px`,
-  );
-  useEffect(() => { // solving mobile vh issue
-    window.addEventListener('resize', setVh);
-    return () => { window.removeEventListener('resize', setVh); };
-  }, []);
-
-
-  const stopCallback = stopId => { // when stop id changes, reset
-    setUpdateTimestamp('');
-    setApiArrivalError(null);
-    setApiRouteError(null);
-    setArrivals([]);
-    setStopId(stopId);
-  };
+    (apiArrivalError || apiArrivalError === null) && throttledGetArrivals(stop.id);
+    (apiRouteError || apiRouteError === null) && throttledGetRoutes(stop.id);
+  }, [apiArrivalError, apiRouteError]);
 
   const refreshHandler = () => {
-    if(!stopId) return;
-    getArrivals(stopId);
-    getRoutes(stopId);
+    getArrivals(stop.id);
+    getRoutes(stop.id);
   };
-
-  const loadMapHandler = () => setLoadMap(true);
 
   const curatedArrivals = useMemo(() => arrivals.map(({route = '', minutes = ''}) => {
     const foundRoute = (routes || arr).find(r => r.id === route) || obj;
@@ -75,20 +39,14 @@ const Index = () => {
   console.log(updateTimestamp, routes, arrivals);
   return (
     <div className={s.app}>
-      {loadMap
-        ? <Map stopCallback={stopCallback} />
-        : <div className={s.prompt} onClick={loadMapHandler}>Load map</div>}
-      {stopId &&
-        <div className={s.dataContainer}>
-          <div className={s.data}>
-            {`${stop.title || ''}`}
-            <p>{`${stop.street ? `(${stop.street})` : ''}`}</p>
-            <ul>{curatedArrivals.map(a => <li key={a.join()}>{a[0]}<p>{a[1]}</p></li>)}</ul>
-            <div className={s.button} onClick={refreshHandler}>Refresh</div>
-            {updateTimestamp && <p className={s.timestamp}>{`Last updated: ${updateTimestamp}`}</p>}
-          </div>
+      <div className={s.dataContainer}>
+        <div className={s.data}>
+          {`${stop.title} (${stop.id})`}
+          <ul>{curatedArrivals.map(a => <li key={a.join()}>{a[0]}<p>{a[1]}</p></li>)}</ul>
+          <div className={s.button} onClick={refreshHandler}>Refresh</div>
+          {updateTimestamp && <p className={s.timestamp}>{`Last updated: ${updateTimestamp}`}</p>}
         </div>
-      }
+      </div>
     </div>
   );
 };
