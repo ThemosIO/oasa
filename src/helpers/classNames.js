8033b959f913744377da9fe15a85395c9aed97f4
ThemosIO <hello@themos.io>
2020-02-05 09:31:39 +0200

    updates

--- src/helpers/classNames.js
+++
@@ -1,4 +0,0 @@
-const classNames = (...classes) =>
-  classes.filter(c => !!c && (typeof c === 'string' || c instanceof String)).join(' ');
-
-export default classNames;
--- src/helpers/remToPx.js
+++
@@ -1,5 +0,0 @@
-const ratio = parseFloat(getComputedStyle(document.documentElement).fontSize);
-const remToPx = rem => rem * ratio;
-const pxToRem = px => px / ratio;
-
-export { remToPx, pxToRem };
